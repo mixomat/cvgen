d36bb9172b8ca34d43e62c624911dda4dc1d63cd
Marc Weinberger <marc.weinberger@me.com>
2012-01-20 11:38:10 +0100

    initial checking

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cvgen::Application.config.secret_token = '57f4627bbaf56b786c538e99ee4585f238f1f0cf179914070455b85bb5c7f2b6d2b46be34244ea3661880772ff60bfdb09382b98d410e69d087ddffe646b9ae3'
