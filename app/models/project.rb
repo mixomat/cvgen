7e8774baaafbfc7429a5a3b0e13288c8fa9feb4d
Marc Weinberger <marc.weinberger@me.com>
2012-02-07 23:18:57 +0100

    improved project validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,6 @@
 class Project < ActiveRecord::Base
-  validates :title, :presence => true
-  validates :description, :presence => true
-  validates :customer, :presence => true
-  validates :role, :presence => true
-
+  validates :title, :description, :customer, :role, :technologies, :presence => true
+  
   has_and_belongs_to_many :technologies
-
   accepts_nested_attributes_for :technologies
 end
