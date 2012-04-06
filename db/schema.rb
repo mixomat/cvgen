e71003790f063ee96ba77ed04789f849347f62e5
Marc Weinberger <marc.weinberger@me.com>
2012-04-06 18:47:31 +0200

    added remember_token to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120406114851) do
+ActiveRecord::Schema.define(:version => 20120406163554) do
 
   create_table "projects", :force => true do |t|
     t.string   "title"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(:version => 20120406114851) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,6 +23,7 @@ describe User do
   it { should respond_to :password }
   it { should respond_to :password_confirmation }
   it { should respond_to :authenticate }
+  it { should respond_to :remember_token }
   it { should be_valid }
 
   describe "when name is not present" do
@@ -99,4 +100,10 @@ describe User do
     end
 
   end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
 end
