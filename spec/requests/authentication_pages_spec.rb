56c14e91d9988fbc185dd6777d52596917ff3dc5
Marc Weinberger <marc.weinberger@me.com>
2012-04-07 12:41:44 +0200

    improved accounts menu

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -32,11 +32,11 @@ describe "Authentication" do
       end
 
       it { should have_selector('title', text: user.name) }
-      it { should have_link('Profile', href: us er_path(user)) }
+      it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Logout', href: logout_path) }
       it { should_not have_link('Login', href: login_path) }
 
-      describe "followed by signout" do
+      describe "followed by logout" do
         before { click_link "Logout" }
         it { should have_link('Login') }
       end
