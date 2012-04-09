f20e39eeadbf4ff75acca2dbdda65d125919dfd1
Marc Weinberger <marc.weinberger@me.com>
2012-04-09 10:59:02 +0200

    fixed page title on technology#index

--- spec/requests/technology_pages_spec.rb
+++ spec/requests/technology_pages_spec.rb
@@ -3,10 +3,23 @@ require 'spec_helper'
 describe "Technology" do
 
   subject { page }
+  before { visit technologies_path }
 
   describe "list" do
-    before { visit technologies_path }
 
-    it { should have_selector("title", :text => "Technologies")}
+    it { should have_selector("title", :text => "Technologies") }
+    it { should have_selector("h2", :text => "Technologies") }
+    it { should_not have_selector("#notice", :text => "Technology was successfully created.") }
+  end
+
+  describe "create" do
+
+    describe "with valid information" do
+      before { fill_in "technology_name", :with => "Rails" }
+
+      it "should create a new technology" do
+        expect { click_button "Create Technology" }.to change(Technology, :count)
+      end
+    end
   end
 end
