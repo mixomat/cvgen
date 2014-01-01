313b580fab412bdf976c25f44c1affe9331b37d7
Marc Weinberger <marc.weinberger@me.com>
2014-01-01 12:34:32 +0100

    migrated projects_controller to strong_paramets

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -23,15 +23,16 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project = Project.new(params[:project])
-    flash[:notice] = "Project was succesfully created." if @project.save
+    @project = Project.new(project_params)
+    flash[:notice] = 'Project was successfully created.' if @project.save
 
     respond_with @project, :location => projects_path
   end
 
+
   def update
     @project = Project.find(params[:id])
-    flash[:notice] = "Project was successfully updated." if @project.update_attributes(params[:project])
+    flash[:notice] = 'Project was successfully updated.' if @project.update_attributes(project_params)
 
     respond_with @project, :location => projects_path
   end
@@ -43,5 +44,11 @@ class ProjectsController < ApplicationController
     respond_with @project
   end
 
+  private
+  def project_params
+    params.require(:project).permit(:title, :description, :customer, :role, :start_date, :end_date)
+  end
 
 end
+
+
