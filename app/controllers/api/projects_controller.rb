c83a5d6a9e6b854370b1dae01946ccea72e691b8
Marc Weinberger <marc.weinberger@me.com>
2014-01-03 01:22:11 +0100

    added CORS header for api access

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,10 +1,19 @@
 module Api
   class ProjectsController < ApplicationController
+    before_filter :allow_cross_domain_access
 
     def index
       @projects = Project.all
       # TODO use jbuilder template for fine grained json output
       render json: @projects
+    end
+
+    private
+    def allow_cross_domain_access
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      response.headers['Access-Control-Request-Method'] = '*'
+      response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     end
 
   end
